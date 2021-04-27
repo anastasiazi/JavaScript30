a4ecc57ee6d14e15ef7c825fb22fcf5590b546b2
anastasiazi <anastasiiiazi@gmail.com>
2021-04-27 20:03:55 +0300

    feat: css edited + sounds added

--- drum-kit/app.js
+++ drum-kit/app.js
@@ -1,1 +1,12 @@
-console.log('hello');
+// audio content
+const AUDIOS = {
+    '65': '',
+    '83': './assets/audio/2.mp3',
+    '68': './assets/audio/3.mp3',
+    '70': './assets/audio/4.mp3',
+    '71': './assets/audio/5.mp3',
+    '72': './assets/audio/6.mp3',
+    '74': './assets/audio/7.mp3',
+    '75': './assets/audio/8.mp3',
+    '76': './assets/audio/9.mp3'
+}
