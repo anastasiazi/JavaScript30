8fa583d9bd56d4618829ce3da6b973988dcf21ef
anastasiazi <anastasiiiazi@gmail.com>
2021-04-27 21:06:57 +0300

    feat: sounds were connected to keys

--- drum-kit/app.js
+++ drum-kit/app.js
@@ -1,12 +1,11 @@
 // audio content
-const AUDIOS = {
-    '65': '',
-    '83': './assets/audio/2.mp3',
-    '68': './assets/audio/3.mp3',
-    '70': './assets/audio/4.mp3',
-    '71': './assets/audio/5.mp3',
-    '72': './assets/audio/6.mp3',
-    '74': './assets/audio/7.mp3',
-    '75': './assets/audio/8.mp3',
-    '76': './assets/audio/9.mp3'
-}
+
+window.addEventListener('keydown', function (e) {
+  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+  const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+  if (!audio) return; // stop the function 
+  audio.currentTime = 0;
+  audio.play();
+  key.classList.add('playing')
+});
+
